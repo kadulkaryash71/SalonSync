d5293665ba9c9d596ce70d1cd48aa75f7dee5cfe
kadulkaryash71 <yashkadulkar@gmail.com>
2025-01-30 18:29:03 +0530

    Navbar corrections

--- data_collector/settings.py
+++ data_collector/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
@@ -131,6 +132,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
